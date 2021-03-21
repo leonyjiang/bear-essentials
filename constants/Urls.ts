789ab1c012d1c89ab51c726c27b6743c39984529
Leon Jiang <35908040+leonyjiang@users.noreply.github.com>
2021-03-20 23:57:24 -0400

    Add graph endpoint URL constant

---
+++ constants/Urls.ts
@@ -0,0 +1,5 @@
+const graphEndpointUrl = "https://api-2cu446h72q-uc.a.run.app/graphql";
+
+export default {
+  graphEndpointUrl,
+};
--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,4 @@
 export { default as Colors } from "./Colors";
 export { default as Layout } from "./Layout";
 export { default as Names } from "./Names";
+export { default as Urls } from './Urls';
